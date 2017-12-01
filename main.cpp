c084dc652ca6f129fba9010a71f87711574b1871
Melina Lopes <melinalps@gmail.com>
2017-12-01 16:47:27 -0200

    .

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 #include <algorithm>
 #include <fstream>
 #include <ctime>
-
 using namespace std;
-extern "C"
-{
+extern "C" {
 #include "lp.h"
 #include "vint_set.h"
 #include "clique_extender.h"
@@ -15,27 +13,74 @@ extern "C"
 }
 
 
-int main( int argc, const char **argv ) {
-    int n=0, rowsRemoved=0, rowsAdded=0, contadorNome=0, start=clock();
-    char* name = new char[10];
-    string arqSaida = argv[1], arqPrint = argv[1];
+void clq_preproccess(LinearProgram *mip, string file);
+
+
+
+int main(int argc, const char **argv) {
+
+    string arqSaida = argv[1];
     while( arqSaida.back() != '.') {
-        arqSaida.pop_back(); arqPrint.pop_back();
+        arqSaida.pop_back();
     }
-    arqSaida.pop_back(); arqPrint.pop_back();
-    arqSaida += "_new.lp"; arqPrint += "_print.txt";
+    arqSaida.pop_back();
+    arqSaida += "_new.mip";
+
+
+
+    int startLeitura=clock();
 
     LinearProgram *lp = lp_create();
     lp_read(lp, argv[1]);
 
-    CGraph *cg = build_cgraph_lp(lp);
-    const int nVertices = cgraph_size(cg);
-    int costs[nVertices];
+    int endLeitura=clock();
 
+
+
+    clq_preproccess(lp, argv[1]);
+
+
+
+    lp_write_lp(lp, arqSaida.c_str());
+
+
+    lp_free(&lp);
+
+    return 0;
+}
+
+
+void clq_preproccess(LinearProgram *mip, string file) {
+    int n=0, rowsRemoved=0, rowsAdded=0, contadorNome=0;
+    int startRest, endRestr, startClique, endClique, start=clock();
+    double timeClique=0.0, timeRestr=0.0;
+    char* name = new char[10];
+
+    string arqPrint = file;
+    while( arqPrint.back() != '.') {
+        arqPrint.pop_back();
+    }
+    arqPrint.pop_back();
+    arqPrint += "_print.txt";
+
+
+
+    int startGraph=clock();
+
+    CGraph *cg = build_cgraph_lp(mip);
+    const int nVertices = cgraph_size(cg);
+
+    int endGraph=clock();
+
+
+
+    int startPreprocess=clock();
+
+    int costs[nVertices];
     fill(costs, costs + nVertices, 1); /* preenchendo o custo de cada variavel com valor 1 */
 
-    int nRes = lp_rows(lp);
-    int nVar = lp_cols(lp);
+    int nRes = lp_rows(mip);
+    int nVar = lp_cols(mip);
 
     int* rowsToRemove = new int[nRes];
     int* idx = new int[nVar];
@@ -43,32 +88,37 @@ int main( int argc, const char **argv ) {
     fill(coef, coef+nVar, 1.0);
     int resToCheck[nRes];
     for(int i=0; i<nRes; i++) {
-        if(lp_sense(lp, i) == 'L' && lp_rhs(lp, i) == 1) {
+        if(lp_sense(mip, i) == 'L' && lp_rhs(mip, i) == 1) {
             resToCheck[i] = 1;
-        } else if(lp_sense(lp, i) == 'E' && lp_rhs(lp, i) == 1) {
+        } else if(lp_sense(mip, i) == 'E' && lp_rhs(mip, i) == 1) {
             resToCheck[i] = 2;
         } else {
             resToCheck[i] = 0;
         }
     }
+    int endPreprocess=clock();
 
 
 
     ofstream saida(arqPrint);
+
 
     IntSet clq, clique, res;
     vint_set_init( &clq );
     vint_set_init( &clique );
     vint_set_init( &res );
 
+
     int startFor=clock();
     for(int k = 0; k < nRes; k++) {
 
         if(resToCheck[k] > 0) {
 
+            n = lp_row(mip, k, idx, coef);
 
-            n = lp_row(lp, k, idx, coef);
 
+
+            startClique=clock();
 
             vint_set_clear(&clique);
             vint_set_add(&clique, idx, n);
@@ -84,13 +134,17 @@ int main( int argc, const char **argv ) {
             int cliqueW = n;
             int status = clqe_extend(clqe, cg, &clique, cliqueW, CLQEM_EXACT);
 
+            endClique=clock();
+            timeClique += (endClique-startClique)/double(CLOCKS_PER_SEC)*1000;
+
+
             if (status > 0) {
 
-                printf("\n%s - lifting module found %d new cliques:\n", lp_row_name(lp, k, name), status);
+                printf("\n%s - lifting module found %d new cliques:\n", lp_row_name(mip, k, name), status);
                 saida << "Restricao " << k << ", " << status << " novos cliques\n";
 
-                printf("   Original (%d): ", lp_row(lp, k, idx, coef));
-                saida << "    Original" << " (" << lp_row(lp, k, idx, coef) << "): ";
+                printf("   Original (%d): ", lp_row(mip, k, idx, coef));
+                saida << "    Original" << " (" << lp_row(mip, k, idx, coef) << "): ";
                 for (int j = 0; j < n; j++) {
                     saida << idx[j] << " ";
                     printf("%d ", idx[j]);
@@ -98,12 +152,15 @@ int main( int argc, const char **argv ) {
                 saida << "\n";
                 printf("\n");
 
+
                 /* novos cliques sao obtidos por meio da consulta seguinte */
                 const CliqueSet *clqSet = clqe_get_cliques(clqe);
 
 
+                startRest=clock();
                 for (int i=0; i<clq_set_number_of_cliques(clqSet); i++) {
 
+                    // nao tem complemento
                     if(clq_set_clique_elements(clqSet, i)[clq_set_clique_size(clqSet, i)-1] < nVar) {
 
                         printf("   %d (%d): ", i, clq_set_clique_size(clqSet, i));
@@ -120,29 +177,30 @@ int main( int argc, const char **argv ) {
                         vint_set_clear( &clq );
                         vint_set_add(&clq, clq_set_clique_elements(clqSet, i), clq_set_clique_size(clqSet, i));
 
-                        for (int j = 0; j < nRes; j++) {
+                        for (int j=0; j<nRes; j++) {
 
                             if (resToCheck[j] > 0) {
 
                                 //pega a j-esima restricao
                                 vint_set_clear(&res);
-                                int n2 = lp_row(lp, j, idx, coef);
+                                int n2 = lp_row(mip, j, idx, coef);
                                 vint_set_add(&res, idx, n2);
 
                                 //se o clique domina a restricao
+                                // USAR VETOR DE INCIDENCIA (INVES DA BUSCA BINARIA) -> VERIFICAR DOMINANCIA A MAO!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                                 if (clq_dominates(&clq, &res) == 1) {
                                     if(resToCheck[k] == 2){
                                         string nome = "clq00" + to_string(contadorNome);
                                         contadorNome++; rowsAdded++;
-                                        n = lp_row(lp, j, idx, coef);
-                                        lp_add_row(lp, n, idx, coef, nome.c_str(), 'G', 1);
+                                        n = lp_row(mip, j, idx, coef);
+                                        lp_add_row(mip, n, idx, coef, nome.c_str(), 'G', 1);
                                     }
                                     rowsToRemove[rowsRemoved] = j;
                                     resToCheck[j] = 0;
                                     rowsRemoved++;
-                                    cout << "        domina a restricao " << lp_row_name(lp, j, name) << endl;
-                                    saida << "        domina a restricao " << lp_row_name(lp, j, name) << "\n";
-                                    cout << rowsRemoved << "   " << rowsAdded << endl;
+                                    cout << "        domina a restricao " << lp_row_name(mip, j, name) << endl;
+                                    saida << "        domina a restricao " << lp_row_name(mip, j, name) << "\n";
+                                    //cout << rowsRemoved << "   " << rowsAdded << endl;
                                 }
 
                             }
@@ -154,13 +212,40 @@ int main( int argc, const char **argv ) {
                             string nome = "clq00" + to_string(contadorNome);
                             contadorNome++;
                             rowsAdded++;
-                            lp_add_row(lp, clq_set_clique_size(clqSet, i),
+                            lp_add_row(mip, clq_set_clique_size(clqSet, i),
                                        const_cast<int *>(clq_set_clique_elements(clqSet, i)), coef, nome.c_str(), 'L',
                                        1);
                         }
 
                     }
+
+                        // tem complemento
+                        // FALAR COM O SAMUEL!!
+                    else {
+//                        string nome = "clq00" + to_string(contadorNome);
+//                        contadorNome++;
+//                        rowsAdded++;
+//
+//                        cout << endl << endl << nome << " COMPLEMENTO!!!" << endl << endl; cout.flush();
+//
+//                        int* elem = const_cast<int*>(clq_set_clique_elements(clqSet, i));
+//
+//                        for(int j=0; j<clq_set_clique_size(clqSet, i); j++) {
+//                            if (elem[j] >= nVar) {
+//                                elem[j] = elem[j]-nVar;
+//                                coef[j] = -1.0;
+//                            }
+//                        }
+//
+//
+//                        lp_add_row(mip, clq_set_clique_size(clqSet, i), elem, coef, nome.c_str(), 'L', 1);
+
+
+
+                    }
                 }
+                endRestr=clock();
+                timeRestr += (endRestr-startRest)/double(CLOCKS_PER_SEC)*1000;
 
             }
 
@@ -170,11 +255,15 @@ int main( int argc, const char **argv ) {
 
     }
 
+
     if (rowsRemoved > 0) {
-        lp_remove_rows(lp, rowsRemoved, rowsToRemove);
+        lp_remove_rows(mip, rowsRemoved, rowsToRemove);
     }
 
+
     int endFor=clock();
+
+
 
     saida << "\n\nRestricoes removidas:   " << rowsRemoved
           << "\nRestricoes adicionadas: " << rowsAdded << "\n"
@@ -187,11 +276,31 @@ int main( int argc, const char **argv ) {
          << " (Total: " << (endFor-start)/double(CLOCKS_PER_SEC)*1000 << "ms)" << endl; cout.flush();
 
 
-    lp_write_lp(lp, arqSaida.c_str());
 
+    fstream tempo("0000_tempos.txt", fstream::app);
+    tempo << file << " "
+          //<< "leitura: " << (endLeitura-startLeitura)/double(CLOCKS_PER_SEC)*1000 << " "
+          << "grafo: " << (endGraph-startGraph)/double(CLOCKS_PER_SEC)*1000 << " "
+          << "preprocessamento: " << (endPreprocess-startPreprocess)/double(CLOCKS_PER_SEC)*1000 << " "
+          << "clique: " << timeClique << " "
+          << "restricoes: " << timeRestr << " "
+          << "for: " << (endFor-startFor)/double(CLOCKS_PER_SEC)*1000 << " "
+          << "\n";
+
+
+    fstream resultado("0000_resultados.txt", fstream::app);
+    resultado << file << " "
+              << rowsRemoved << " "
+              << rowsAdded << " "
+              << "\n";
+
+
+
+    resultado.close();
+    tempo.close();
     saida.close();
+
     cgraph_free(&cg);
-    lp_free(&lp);
     vint_set_clean( &clq );
     vint_set_clean( &clique );
     delete[] idx;
@@ -199,5 +308,4 @@ int main( int argc, const char **argv ) {
     delete[](rowsToRemove);
     delete[] name;
 
-    return 0;
 }
